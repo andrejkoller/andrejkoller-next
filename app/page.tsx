7318958cc63e20f093f599ced09f3017a3e10a1e
Andrej Koller <andrejkoller@outlook.com>
2026-04-25 22:33:39 +0200

    refactor: update Home component structure and links, improve styling variables in globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -3,56 +3,56 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="px-4">
-      <span>Andrej Koller</span>
-      <br />
-      <span>
-        Founding Engineer at{" "}
+      <div className="flex flex-col mb-4">
+        <span>Andrej Koller</span>
+        <span>
+          Founding Engineer at{" "}
+          <Link
+            href="https://athanasa.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Athanasa
+          </Link>
+        </span>
+        <span>
+          Latest Projects/Clients:{" "}
+          <Link
+            href={"https://fadinghell.com"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Fading Hell
+          </Link>{" "}
+          •{" "}
+          <Link
+            href={"https://prettyswing.com"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Pretty Swing
+          </Link>
+        </span>
+      </div>
+      <div className="inline">
         <Link
-          href="https://athanasa.com"
+          href={"https://www.linkedin.com/in/andrejkoller"}
           target="_blank"
           rel="noopener noreferrer"
+          className="uppercase text-[0.8em]! text-[#333]!"
         >
-          Athanasa
+          LinkedIn
         </Link>
-      </span>
-      <br />
-      <span>
-        Latest Projects/Clients:{" "}
+        <span className="mx-1">•</span>
         <Link
-          href={"https://fadinghell.com"}
+          href={"https://github.com/andrejkoller"}
           target="_blank"
           rel="noopener noreferrer"
+          className="uppercase text-[0.8em]! text-[#333]!"
         >
-          Fading Hell
-        </Link>{" "}
-        •{" "}
-        <Link
-          href={"https://prettyswing.com"}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Pretty Swing
+          GitHub
         </Link>
-      </span>
-      <br />
-      <br />
-      <Link
-        href={"https://www.linkedin.com/in/andrejkoller"}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="uppercase text-[0.8em]! text-[#333]!"
-      >
-        LinkedIn
-      </Link>{" "}
-      •{" "}
-      <Link
-        href={"https://github.com/andrejkoller"}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="uppercase text-[0.8em]! text-[#333]!"
-      >
-        GitHub
-      </Link>
+      </div>
     </div>
   );
 }
