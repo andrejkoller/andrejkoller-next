4a7c61dffb83f9e4ce7f94f59f24e5a07c4845b2
Andrej Koller <andrejkoller@outlook.com>
2026-05-25 09:34:12 +0200

    feat: add additional header links for art gallery and online shop

--- config/header-links-config.ts
+++ config/header-links-config.ts
@@ -10,8 +10,18 @@ export const headerLinksConfig = [
     href: "https://dev.andrejkoller.com",
   },
   {
+    key: "art-gallery",
+    label: "Art Gallery",
+    href: "https://art.andrejkoller.com",
+  },
+  {
     key: "project-showcase",
     label: "Project Showcase",
     href: "https://projects.andrejkoller.com",
+  },
+  {
+    key: "online-shop",
+    label: "Online Shop",
+    href: "https://shop.andrejkoller.com",
   },
 ];
